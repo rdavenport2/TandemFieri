2c4295edcc8dde15dc528ac44046c5127a154a25
Ruth <rdavenport2@uco.edu>
2017-03-24 13:52:05 -0500

    update diner order history view
    cleaned up diner order history to make space for filter options and to
    view a nicer layout

--- app/src/main/java/com/gmail/dleemcewen/tandemfieri/DinerOrderHistoryActivity.java
+++ app/src/main/java/com/gmail/dleemcewen/tandemfieri/DinerOrderHistoryActivity.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.gmail.dleemcewen.tandemfieri.Adapters.DinerOrderHistoryArrayAdapter;
 import com.gmail.dleemcewen.tandemfieri.Entities.Order;
 import com.gmail.dleemcewen.tandemfieri.Entities.User;
-import com.gmail.dleemcewen.tandemfieri.Logging.LogWriter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,14 +18,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
 
 public class DinerOrderHistoryActivity extends AppCompatActivity {
-    User user;
-    List<Order> ordersList;
-    DatabaseReference mDatabase;
-    ListView orderHistoryView;
+    private User user;
+    private ArrayList<Order> ordersList;
+    private DatabaseReference mDatabase;
+    private ListView orderHistoryView;
+    private DinerOrderHistoryArrayAdapter dinerOrderHistoryArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +51,6 @@ public class DinerOrderHistoryActivity extends AppCompatActivity {
     }
 
     private void openOrder(Order order, int position){
-        LogWriter.log(getApplicationContext(), Level.INFO, "This will open the Order");
         Bundle orderBundle = new Bundle();
         Intent intent = new Intent(DinerOrderHistoryActivity.this, ViewOrderActivity.class);
         orderBundle.putSerializable("Order", order);
@@ -71,11 +68,11 @@ public class DinerOrderHistoryActivity extends AppCompatActivity {
 
                 //everything to do with order list code here
                 for(DataSnapshot owners: dataSnapshot.getChildren()){
-                    LogWriter.log(getApplicationContext(), Level.INFO, "owner #: " + owners.getKey());
+                    //LogWriter.log(getApplicationContext(), Level.INFO, "owner #: " + owners.getKey());
                     for(DataSnapshot orders : owners.getChildren()){
-                        LogWriter.log(getApplicationContext(), Level.INFO, "order #: " + orders.getKey());
+                        //LogWriter.log(getApplicationContext(), Level.INFO, "order #: " + orders.getKey());
                         Order o = orders.getValue(Order.class);
-                       LogWriter.log(getApplicationContext(), Level.INFO, "customer #: " + o.getCustomerId());
+                       //LogWriter.log(getApplicationContext(), Level.INFO, "customer #: " + o.getCustomerId());
                         if(o.getCustomerId() != null){
                             if(o.getCustomerId().equals(user.getAuthUserID())){
                                 ordersList.add(o);
@@ -88,11 +85,8 @@ public class DinerOrderHistoryActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "You have no orders to display.", Toast.LENGTH_LONG).show();
                 }else {
 
-                    ArrayAdapter<Order> adapter = new ArrayAdapter<>(
-                            getApplicationContext(),
-                            R.layout.diner_order_history_item,
-                            ordersList);
-                    orderHistoryView.setAdapter(adapter);
+                    dinerOrderHistoryArrayAdapter = new DinerOrderHistoryArrayAdapter(getApplicationContext(), ordersList);
+                    orderHistoryView.setAdapter(dinerOrderHistoryArrayAdapter);
                     }
 
             }//end on data change
